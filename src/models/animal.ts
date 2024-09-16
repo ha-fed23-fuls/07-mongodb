e3cb50ebfea542b5a285241d5ea8b8ed167f8bf5
David Andersson <lejonmanen@gmail.com>
2024-09-16 11:35:16 +0200

    Refactor

---
+++ src/models/animal.ts
@@ -0,0 +1,6 @@
+export interface Animal {
+	// _id: ObjectId;  <- läggs till av WithId<>
+	species: string;
+	factoid: string;
+	score: number;
+}
--- src/mongodb-examples.ts
+++ src/mongodb-examples.ts
@@ -8,6 +8,7 @@ const test: string | undefined = process.env.TEST
 console.log('Test env-fil: ' + test)
 
 import { MongoClient, Db, Collection, ObjectId, WithId } from 'mongodb'
+import { Animal } from './models/animal.js'
 
 async function connect() {
 	const con: string | undefined = process.env.CONNECTION_STRING
@@ -21,15 +22,7 @@ async function connect() {
 		const db: Db = await client.db('exercises')
 		const col: Collection<Animal> = db.collection<Animal>('animalFacts')
 
-		// const filter = { }  // tomt objekt väljer ut alla
-		const filter = { score: { $lt: 32 } }  // score < 32
-		const found: WithId<Animal> | null = await col.findOne(filter)
-		// TODO: validera med Joi
-		if( found ) {
-			console.log('Found animal: ', found)
-		} else {
-			console.log('All animals were in hiding...')
-		}
+		findTheElephant(col)
 
 		await client.close()
 	} catch(error: any) {
@@ -38,11 +31,18 @@ async function connect() {
 	console.log('Program executed successfully.')
 }
 
-interface Animal {
-	// _id: ObjectId;  <- läggs till av WithId<>
-	species: string;
-	factoid: string;
-	score: number;
+async function findTheElephant(col: Collection<Animal>): Promise<void> {
+	// const filter = { }  // tomt objekt väljer ut alla
+	const filter = { score: { $lt: 32 } }  // score < 32
+	const found: WithId<Animal> | null = await col.findOne(filter)
+	// TODO: validera med Joi
+	if( found ) {
+		console.log('Found animal: ', found)
+	} else {
+		console.log('All animals were in hiding...')
+	}
 }
+
+
 
 connect()
