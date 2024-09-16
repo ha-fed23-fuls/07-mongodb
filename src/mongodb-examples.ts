a484f3fdd34061a0b792f93c14ee7ca4779a7282
David Andersson <lejonmanen@gmail.com>
2024-09-16 11:28:45 +0200

    Find one

--- src/mongodb-examples.ts
+++ src/mongodb-examples.ts
@@ -7,7 +7,7 @@ CONNECTION_STRING=kopierad från Compass
 const test: string | undefined = process.env.TEST
 console.log('Test env-fil: ' + test)
 
-import { MongoClient, Db, Collection, ObjectId } from 'mongodb'
+import { MongoClient, Db, Collection, ObjectId, WithId } from 'mongodb'
 
 async function connect() {
 	const con: string | undefined = process.env.CONNECTION_STRING
@@ -15,17 +15,31 @@ async function connect() {
 		console.log('ERROR: connection string not found!')
 		return
 	}
-	const client: MongoClient = new MongoClient(con)
+	try {
+		const client: MongoClient = new MongoClient(con)
 
-	const db: Db = await client.db('exercises')
-	const col: Collection<Animal> = db.collection<Animal>('animalFacts')
+		const db: Db = await client.db('exercises')
+		const col: Collection<Animal> = db.collection<Animal>('animalFacts')
 
-	// TODO: filter + findOne
+		// const filter = { }  // tomt objekt väljer ut alla
+		const filter = { score: { $lt: 32 } }  // score < 32
+		const found: WithId<Animal> | null = await col.findOne(filter)
+		// TODO: validera med Joi
+		if( found ) {
+			console.log('Found animal: ', found)
+		} else {
+			console.log('All animals were in hiding...')
+		}
+
+		await client.close()
+	} catch(error: any) {
+		console.log('An error occurred. ' + error.message)
+	}
 	console.log('Program executed successfully.')
 }
 
 interface Animal {
-	_id: ObjectId;
+	// _id: ObjectId;  <- läggs till av WithId<>
 	species: string;
 	factoid: string;
 	score: number;
